565991b88d136537362902bcb30626057a5af30d
noelcv <79803356+noelcv@users.noreply.github.com>
2022-08-29 13:53:10 +0200

    add: Header component

--- ecommerce/src/App.tsx
+++ ecommerce/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
 
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className="app">
+      <Header />
+      {/*Home*/}
     </div>
   )
 }
---
+++ ecommerce/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React, {FunctionComponent} from 'react'
+import './Header.css'
+
+
+const Header: FunctionComponent = () => {
+  return (
+    <div>
+    <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header
