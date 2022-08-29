da30606e76b9100cef693d80cba0123f9a14a98e
noelcv <79803356+noelcv@users.noreply.github.com>
2022-08-29 15:06:28 +0200

    css: Header + MaterialUI

--- ecommerce/src/components/Header.tsx
+++ ecommerce/src/components/Header.tsx
@@ -2,6 +2,8 @@ import React, {FunctionComponent} from 'react'
 import './Header.css'
 import eShop_logo from '../assets/eShop_logo.svg'
 import magnify from '../assets/magnify.svg'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Header: FunctionComponent = () => {
   return (
@@ -9,7 +11,8 @@ const Header: FunctionComponent = () => {
       <img className='header-logo' src={eShop_logo}/>
       <div className="header-search-bar-wrapper">
         <input className="header-search-bar-input" type="text" placeholder="Search"/>
-        <button className="header-search-bar-button"><img src={magnify} width="30"/></button>
+        
+        <button className="header-search-bar-button"><SearchIcon/></button>
       </div>
       <div className="header-nav">
         <div className="header-options">
@@ -23,6 +26,11 @@ const Header: FunctionComponent = () => {
         <div className="header-options">
           <span className="header-option-l1">Your</span>
           <span className="header-option-l2">Subscription</span>
+        </div>
+        <div className="header-option-basket">
+          <ShoppingCartIcon 
+            fontSize='large'
+          />
         </div>
         
       </div>
