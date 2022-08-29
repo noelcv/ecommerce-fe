1556ee4d2722b94b8e40be519c638728e30f2282
noelcv <79803356+noelcv@users.noreply.github.com>
2022-08-29 15:12:54 +0200

    add: Home component

--- ecommerce/src/App.tsx
+++ ecommerce/src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Header from './components/Header'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
-
-
   return (
     <div className="app">
       <Header />
-      {/*Home*/}
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ ecommerce/src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React, {FunctionComponent} from 'react'
+import './Home.css'
+
+const Home: FunctionComponent = () => {
+  return (
+    <div className="home">
+      
+    </div>
+  )
+}
+
+export default Home;
