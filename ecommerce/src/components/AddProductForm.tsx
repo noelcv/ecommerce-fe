42a2cb253f719075634cc02a170a523aa2cf061e
noelcv <79803356+noelcv@users.noreply.github.com>
2022-08-31 14:44:08 +0200

    create AddProductForm component

---
+++ ecommerce/src/components/AddProductForm.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from 'react';
+import './AddProductForm.css';
+
+const AddProductForm: FunctionComponent = () => {
+  return (
+    <div className="add-product-form">
+      <h1>Add Product Form</h1>
+
+      
+      
+      
+    </div>
+  );
+}
+
+export default AddProductForm
--- ecommerce/src/components/Admin.tsx
+++ ecommerce/src/components/Admin.tsx
@@ -1,10 +1,15 @@
 import React, { FunctionComponent } from 'react';
+import AddProductForm from './AddProductForm';
 import './Admin.css';
 
 const Admin: FunctionComponent = () => {
   return (
     <div className="admin">
-      <h1>Admin</h1>
+      <h1>Admin Panel</h1>
+      <AddProductForm />
+      
+      
+      
     </div>
   );
 }
