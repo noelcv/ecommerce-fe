13d01d9a8b2c2a2b08e675478bbb48543160093d
noelcv <79803356+noelcv@users.noreply.github.com>
2022-08-31 14:39:01 +0200

    feat: initial Admin Page / component / routing

---
+++ ecommerce/src/components/Admin.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+import './Admin.css';
+
+const Admin: FunctionComponent = () => {
+  return (
+    <div className="admin">
+      <h1>Admin</h1>
+    </div>
+  );
+}
+
+export default Admin
