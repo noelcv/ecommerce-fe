06f20d72ac65b23b31b2889776adedb3ff1f44a8
noelcv <79803356+noelcv@users.noreply.github.com>
2022-09-22 16:17:23 +0200

    graphQLAPI: implement deleteProduct service

---
+++ ecommerce/src/services/product/deleteProduct.service.ts
@@ -0,0 +1,34 @@
+const GRAPHQL_API_URL: string = "http://localhost:3000/graphql";
+import { ProductType } from "../../types/ProductType";
+
+export const deleteProduct = async (product: ProductType) => {
+  try {
+    return await fetch(GRAPHQL_API_URL, {
+      method: "POST",
+      headers: {"Content-type":"application/json"},
+      body: JSON.stringify({
+        variables: {deleteProductId: product.id},
+        query: `
+        mutation DeleteProduct($deleteProductId: String!) {
+          deleteProduct(id: $deleteProductId) {
+            code
+            success
+            message
+            product {
+              id
+              name
+              description
+              price
+              currency
+              createdAt
+              updatedAt
+            }
+          }
+        }`
+      })
+    })
+  }
+  catch(error) {
+    console.log('Error at deleteProduct Service: ', error)
+  } 
+}
--- ecommerce/src/services/product/product.ts
+++ ecommerce/src/services/product/product.ts
@@ -5,29 +5,3 @@ const BASE_URL: string = "http://localhost:3000/products";
 const GRAPHQL_API_URL: string = "http://localhost:3000/graphql";
 
 
-export const editProduct = async (product: ProductType) => {
-  try {
-    const editedProduct = await fetch(`${BASE_URL}/${product.id}`, {
-      method: "PATCH",
-      headers: {"Content-type":"application/json"},
-      body: JSON.stringify(product)
-    })
-    return await editedProduct;
-  }
-  catch(error) {
-    console.log('Error at editProduct Service: ', error)
-  }
-}
-
-export const deleteProduct = async (product: ProductType) => {
-  try {
-    const deletedProduct = await fetch(`${BASE_URL}/${product.id}`, {
-      method: "DELETE",
-      headers: {"Content-type":"application/json"},
-    })
-    return await deletedProduct;
-  }
-  catch(error) {
-    console.log('Error at deleteProduct Service: ', error)
-  } 
-}
