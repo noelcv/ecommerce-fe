e965923f210a4896e57f51e48e668c1b7f5af92e
noelcv <79803356+noelcv@users.noreply.github.com>
2022-10-03 14:06:32 +0200

    test: reducers functionality at subtotal component

--- ecommerce/src/components/Subtotal.tsx
+++ ecommerce/src/components/Subtotal.tsx
@@ -1,16 +1,29 @@
 import { FunctionComponent } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { resetBasket } from '../redux/reducers/product/basketSlice';
+import { resetBasketCounter } from '../redux/reducers/product/counterSlice';
+import { resetSubtotal } from '../redux/reducers/product/subtotalSlice';
 import { RootState } from '../redux/store';
 
 const Subtotal: FunctionComponent = () => {
   const items = useSelector((state: RootState) => state.counter.value);
   const subtotal = useSelector((state: RootState) => state.subtotal.value);
-  console.log(subtotal, 'subtotal');
+  const dispatch = useDispatch()
+  
+  //TODO: make it async and add payment middleware
+  const completeOrder = () => {
+    dispatch(resetBasket())
+    dispatch(resetBasketCounter())
+    dispatch(resetSubtotal())
+  }
+  
   return (
     <div className="flex flex-col space-x-2 w-60 md:w-96 bg-slate-100 border-gray-200 shadow-lg border-solid p-2 rounded-sm">
       <p>Subtotal({items} items):</p>
       <strong>{subtotal} EUR</strong>
-      <button className="bg-zinc-800">Proceed to checkout</button>
+      <button
+      onClick={completeOrder}
+      className="bg-zinc-800">Proceed to checkout</button>
     </div>
   )
 };
